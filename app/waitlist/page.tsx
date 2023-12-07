1d7000a31afb369fe7417cf577581cdfbbd83498
mohammadrz003 <rezaiimohammad003@gmail.com>
2023-12-08 01:01:52 +0330

    [FIX] enabled the join waitlist button

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -46,39 +46,39 @@ const Page = (props: Props) => {
   return (
     <Layout>
       <div className="min-h-[calc(100vh-76px)] p-5 flex justify-center items-center">
-        <div className="w-full flex flex-col items-center">
+        <div className="flex flex-col items-center w-full">
           {isSignupSiccessful ? (
             <div className="w-full max-w-[544px]">
-              <h1 className="text-center mb-10 text-2xl">
+              <h1 className="mb-10 text-2xl text-center">
                 Successfully signed up for Waitlist
               </h1>
               <div className="grid grid-cols-2 gap-3">
-                <div className="border border-slate-700 rounded-lg p-5 text-center flex flex-col justify-center">
-                  <p className="text-slate-400 text-xs mb-2">Your Position</p>
-                  <p className="text-slate-300 text-sm">
+                <div className="flex flex-col justify-center p-5 text-center border rounded-lg border-slate-700">
+                  <p className="mb-2 text-xs text-slate-400">Your Position</p>
+                  <p className="text-sm text-slate-300">
                     {userWaitlistData?.userData?.verified
                       ? userWaitlistData?.userData?.priority
                       : `This waitlist requires you to verify your email. To join, click the link we sent to ${userWaitlistData?.userData?.email}.`}
                   </p>
                 </div>
-                <div className="border border-slate-700 rounded-lg p-5 text-center flex flex-col justify-center">
-                  <p className="text-slate-400 text-xs mb-2">
+                <div className="flex flex-col justify-center p-5 text-center border rounded-lg border-slate-700">
+                  <p className="mb-2 text-xs text-slate-400">
                     People on Waitlist
                   </p>
-                  <p className="text-slate-300 text-sm">
+                  <p className="text-sm text-slate-300">
                     {userWaitlistData?.waitlist?.statistics?.current_signups}
                   </p>
                 </div>
-                <div className="col-span-2 border border-slate-700 rounded-lg p-5 text-center flex flex-col justify-center">
-                  <p className="text-slate-400 text-xs mb-2">Referral Link</p>
-                  <p className="text-slate-300 text-sm">
+                <div className="flex flex-col justify-center col-span-2 p-5 text-center border rounded-lg border-slate-700">
+                  <p className="mb-2 text-xs text-slate-400">Referral Link</p>
+                  <p className="text-sm text-slate-300">
                     {userWaitlistData?.userData?.referral_link}
                   </p>
                 </div>
-                <div className="col-span-2 border border-slate-700 rounded-lg p-5 text-center flex flex-col justify-center">
-                  <p className="text-slate-400 text-xs mb-2">Leaderboard</p>
+                <div className="flex flex-col justify-center col-span-2 p-5 text-center border rounded-lg border-slate-700">
+                  <p className="mb-2 text-xs text-slate-400">Leaderboard</p>
 
-                  <ul className="text-slate-300 text-sm">
+                  <ul className="text-sm text-slate-300">
                     {userWaitlistData?.waitlistLeaderboard?.leaderboard?.map(
                       (item: any, index: number) => (
                         <li key={index}>
@@ -92,14 +92,14 @@ const Page = (props: Props) => {
             </div>
           ) : (
             <div className="w-full max-w-[544px] text-center">
-              <h1 className="text-center mb-10 text-2xl">
+              <h1 className="mb-10 text-2xl text-center">
                 {hasSignedUp ? "Check your rank" : "Sign up for Poply Waitlist"}
               </h1>
               <form onSubmit={handleWaitlistSubmit} className="w-full">
-                <div className="w-full flex flex-col sm:flex-row space-y-3 sm:space-y-0">
+                <div className="flex flex-col w-full space-y-3 sm:flex-row sm:space-y-0">
                   <input
                     placeholder="Enter your email"
-                    className="w-full rounded-l-lg rounded-r-lg sm:rounded-r-none p-3 text-slate-900"
+                    className="w-full p-3 rounded-l-lg rounded-r-lg sm:rounded-r-none text-slate-900"
                     name="email"
                     value={waitlistEmail}
                     onChange={handleWaitlistEmailChange}
@@ -107,7 +107,7 @@ const Page = (props: Props) => {
                   <button
                     disabled={isLoading}
                     type="submit"
-                    className="p-3 bg-lime-400 rounded-l-lg rounded-r-lg sm:rounded-l-none text-black disabled:opacity-70 disabled:cursor-not-allowed sm:whitespace-nowrap"
+                    className="p-3 text-black rounded-l-lg rounded-r-lg bg-lime-400 sm:rounded-l-none disabled:opacity-70 disabled:cursor-not-allowed sm:whitespace-nowrap"
                   >
                     Join Waitlist
                   </button>
